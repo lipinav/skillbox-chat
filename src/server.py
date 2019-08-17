76f2ac3b271b0a8ca78807031fd201e9c4451746
a.lipin <a.lipin@galaksion.com>
2019-08-17 14:26:20 +0300

    homework-day-2

--- src/server.py
+++ src/server.py
@@ -8,6 +8,7 @@
 from twisted.internet import reactor
 from twisted.protocols.basic import LineOnlyReceiver
 from twisted.internet.protocol import ServerFactory, connectionDone
+from time import gmtime, strftime
 
 
 class Client(LineOnlyReceiver):
@@ -33,8 +34,10 @@ class Client(LineOnlyReceiver):
 
         self.ip = self.transport.getPeer().host  # записываем IP адрес клиента
         self.factory.clients.append(self)  # добавляем в список клиентов фабрики
+#        for i in self.factory.clients:
+#            print(i, end="")
 
-        self.sendLine("Welcome to the chat!".encode())  # отправляем сообщение клиенту
+#        self.sendLine("Welcome to the chat!".encode())  # отправляем сообщение клиенту
 
         print(f"Client {self.ip} connected")  # отображаем сообщение в консоли сервера
 
@@ -59,19 +62,53 @@ class Client(LineOnlyReceiver):
         """
 
         message = line.decode()  # раскодируем полученное сообщение в строку
+        newlogin = message.replace("login:", "")
 
+#        print(f"USERS0: COUNT: {len(self.factory.clients)}")
+#        print(f"LOGIN0: {self.login}")
         # если логин еще не зарегистрирован
         if self.login is None:
             if message.startswith("login:"):  # проверяем, чтобы в начале шел login:
-                self.login = message.replace("login:", "")  # вырезаем часть после :
 
-                notification = f"New user: {self.login}"  # формируем уведомление о новом клиенте
-                self.factory.notify_all_users(notification)  # отсылаем всем в чат
+                user_count = 0
+                user_exists = False
+
+                for user in self.factory.clients:    #check dublicates
+#                    print(f"USER NUMBER: {user_count}")
+#                    print(f"USER1 COUNT: {len(self.factory.clients)}")
+#                    print(f"OLD LOGIN: {user.login}")  #old users
+#                    print(f"NEW LOGIN: {self.login}")  #new user
+#                    print(f"NEWLOGIN: {newlogin}")
+                    if newlogin == user.login:
+                        user_exists = True
+                        break
+                    user_count += 1
+
+                if user_exists:
+                    print(f"NEW USER EXISTS: {self.login}. REMOVE!")
+                    self.sendLine("Login exists, try again".encode())
+                    self.transport.loseConnection(self)
+                else:
+                    self.login = newlogin
+                    # формируем уведомление о новом клиенте
+                    notification = f"New user: {self.login}"
+                    # отсылаем всем в чат
+                    self.factory.notify_about_new_users(notification)
+                    self.sendLine("Welcome to the chat!".encode())  # отправляем сообщение клиенту
+
+                    #send 10 messages to new client
+                    if self.factory.chat:
+                        self.sendLine("Last 10 messages:".encode())
+                        for message in self.factory.chat[-10:]:
+                            self.sendLine(message)
+#                            print(message)
             else:
-                self.sendLine("Invalid login".encode())  # шлем уведомление, если в сообщении ошибка
+                # шлем уведомление, если в сообщении ошибка
+                self.sendLine("Invalid login".encode())
         # если логин уже есть и это следующее сообщение
         else:
-            format_message = f"{self.login}: {message}"  # форматируем сообщение от имени клиента
+            # форматируем сообщение от имени клиента
+            format_message = f"{self.login}: {message}"
 
             # отсылаем всем в чат и в консоль сервера
             self.factory.notify_all_users(format_message)
@@ -83,6 +120,7 @@ class Server(ServerFactory):
 
     clients: list  # список клиентов
     protocol = Client  # протокол обработки клиента
+    chat: list = []   #chat list
 
     def __init__(self):
         """
@@ -94,6 +132,7 @@ class Server(ServerFactory):
 
         self.clients = []  # создаем пустой список клиентов
 
+
         print("Server started - OK")  # уведомление в консоль сервера
 
     def startFactory(self):
@@ -101,13 +140,28 @@ class Server(ServerFactory):
 
         print("Start listening ...")  # уведомление в консоль сервера
 
+    def notify_about_new_users(self, message: str):
+        """
+        Отправка сообщения всем клиентам чата
+        about new user
+        """
+
+        data = message.encode()  # закодируем текст в двоичное представление
+
+        # отправим всем подключенным клиентам
+        for user in self.clients:
+            user.sendLine(data)
+
     def notify_all_users(self, message: str):
         """
         Отправка сообщения всем клиентам чата
         :param message: Текст сообщения
         """
 
-        data = message.encode()  # закодируем текст в двоичное представление
+        message_with_time = strftime("%X", gmtime()) + "  " + message
+        # закодируем текст в двоичное представление
+        data = message_with_time.encode()
+        self.chat.append(data)
 
         # отправим всем подключенным клиентам
         for user in self.clients:
